fa37810b780367caf2ffeefb53dc76b8f41da43b
Ayushh-patell <ayush.patel.code@gmail.com>
2026-05-02 18:18:13 +0530

    nodemailer import

--- routes/ZeejayRoute.js
+++ routes/ZeejayRoute.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const nodemailer = require('nodemailer');
 require('dotenv').config();
 const SDK = require('@ringcentral/sdk').SDK;
 // Initialize RingCentral SDK
