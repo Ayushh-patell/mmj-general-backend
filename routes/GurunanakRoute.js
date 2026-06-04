f0f78e154f92611b5e46128ed87c6b3c0b018475
Ayushh-patell <ayush.patel.code@gmail.com>
2026-06-04 11:30:31 +0530

    errir fix

--- routes/GurunanakRoute.js
+++ routes/GurunanakRoute.js
@@ -2,7 +2,6 @@
 
 const express = require("express");
 const axios = require("axios");
-const express = require('express');
 const router = express.Router();
 require("dotenv").config();
 const nodemailer = require('nodemailer');
